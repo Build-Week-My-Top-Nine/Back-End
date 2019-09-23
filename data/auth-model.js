db252d84ffe497915a1efbd87b08d2392d77129c
Your Name <tfikrie@yahoo.com>
2019-09-23 19:51:20 -0400

    Auth model and  database table created

---
+++ data/auth-model.js
@@ -0,0 +1,23 @@
+const db = require('./dbConfig.js')
+
+module.exports = {
+    register,
+    login,
+    findByregisterId
+  };
+
+  async function register(user) {
+    const [id] = await db('mytopnineusers').insert(user);
+  
+    return findByregisterId(id);
+  }
+
+  function login(username){
+    return db('mytopnineusers').where({username})
+  }
+
+  function findByregisterId(id) {
+    return db('mytopnineusers')
+      .where({ id })
+      .first();
+  }
